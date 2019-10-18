0fa7883875f39595ab6b8738741d4a40d14eae3d
Sangwa-C <chelseasabinesangwa@gmail.com>
2019-10-18 11:18:43 +0200

    added the call clothes to process the activity

--- app/src/main/java/com/chel/sangwa_sytles/ClothesActivity.java
+++ app/src/main/java/com/chel/sangwa_sytles/ClothesActivity.java
@@ -3,6 +3,7 @@ package com.chel.sangwa_sytles;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
@@ -12,9 +13,13 @@ import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.util.List;
+
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class ClothesActivity extends AppCompatActivity {
     @BindView(R.id.clothesTextView) TextView mClothesTextView;
@@ -41,13 +46,44 @@ public class ClothesActivity extends AppCompatActivity {
 
         ButterKnife.bind(this);
         Intent cheHome = getIntent();
-        String clothType = cheHome.getStringExtra("clothType");
+        String location = cheHome.getStringExtra("clothType");
+
         sangwa_stylesArrayAdapter cece = new sangwa_stylesArrayAdapter(this, android.R.layout.simple_list_item_1, clothes, prices);
 
         YelpApi client = YelpClient.getClient();
 
-        Call<YelpBusinessesSearchResponse> call = client.getRestaurants(location, "restaurants");
+        Call<YelpBusinessesSearchResponse> call = client.getRestaurants(location, "clothes");
 
+        call.enqueue(new Callback<YelpBusinessesSearchResponse>() {
+
+
+            @Override
+            public void onResponse(Call<YelpBusinessesSearchResponse> call, Response<YelpBusinessesSearchResponse> response) {
+                if (response.isSuccessful()) {
+                    List<Business> clothesList = response.body().getBusinesses();
+                    String[] clothes = new String[clothesList.size()];
+                    String[] categories = new String[clothesList.size()];
+
+                    for (int i = 0; i < clothes.length; i++){
+                        clothes[i] = clothesList.get(i).getName();
+                    }
+
+                    for (int i = 0; i < categories.length; i++) {
+                        Category category = clothesList.get(i).getCategories().get(0);
+                        categories[i] = category.getTitle();
+                    }
+
+                    ArrayAdapter adapter = new sangwa_stylesArrayAdapter(ClothesActivity.this, android.R.layout.simple_list_item_1, clothes, categories);
+                    mClothList.setAdapter(adapter);
+
+                }
+            }
+
+            @Override
+            public void onFailure(Call<YelpBusinessesSearchResponse> call, Throwable t) {
+
+            }
+        });
 
         mClothList.setAdapter(cece);
         mClothList.setOnItemClickListener(new AdapterView.OnItemClickListener(){
@@ -57,7 +93,7 @@ public class ClothesActivity extends AppCompatActivity {
                 Toast.makeText(ClothesActivity.this, clothes, Toast.LENGTH_LONG).show();
             }
         });
-        mClothesTextView.setText(clothType + " cloth");
+        mClothesTextView.setText(location + " cloth");
     }
 
 
