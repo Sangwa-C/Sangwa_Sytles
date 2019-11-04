e06fa4fb76b2bce379942c360ab378abefc8a2bc
Sangwa-C <chelseasabinesangwa@gmail.com>
2019-11-04 10:28:04 +0200

    last edits

--- app/src/main/java/com/chel/sangwa_sytles/ImagesActivity.java
+++ app/src/main/java/com/chel/sangwa_sytles/ImagesActivity.java
@@ -1,16 +1,14 @@
 package com.chel.sangwa_sytles;
 
-import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ListView;
-import android.widget.TextView;
-import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.chel.sangwa_sytles.ui.ClothesActivity;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
